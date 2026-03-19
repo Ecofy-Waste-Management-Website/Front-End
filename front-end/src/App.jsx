eb421cbece9279dce565511ed4bcc8a9bad4f04a
Ravindu Bandara <ravindu2923@gmail.com>
2026-03-19 21:03:05 +0530

    feat: implement initial React application structure with routing for home and contact pages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,16 +1,10 @@
 import React from 'react'
-<<<<<<< Updated upstream
-import Hero from './Components/Hero-Section/hero'
-import Navbar from './Components/Top-Header-Section/navbar/navbar'
-import Footer from './Components/Footer/footer'
-=======
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './Components/Top-Header-Section/navbar/navbar'
 import Footer from './Components/Footer/footer'
 import Home from './Components/Home/home'
 import ContactUs from './Components/Contact-Us/contactus'
 
->>>>>>> Stashed changes
 export default function App() {
   return (
     <BrowserRouter>
