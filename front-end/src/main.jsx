73fb1951f845f97545dcfb34ce54aa057839d49b
yuthila_banuka <banukadeseram12@gmail.com>
2026-03-19 23:13:04 +0530

    some changes

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -1,15 +1,3 @@
-<<<<<<< Updated upstream
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
@@ -22,4 +10,4 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     </ClerkProvider>
   </React.StrictMode>
 );
->>>>>>> Stashed changes
+
